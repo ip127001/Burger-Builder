e1535e3f19f0d29e383f5ee920dbd2ad86a5d03b
Rohit Kumawat <rkumawat125@gmail.com>
2019-03-15 01:34:22 +0530

    removed the interceptors with axios eject to prevent memory leaks

--- src/hoc/App.js
+++ src/hoc/App.js
@@ -4,11 +4,21 @@ import Layout from './Layout/Layout';
 import BurgerBuilder from '../containers/BurgerBulder/BurgerBuilder';
 
 class App extends Component {
+  state = {
+    show: true
+  }
+  
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({show: false})
+    }, 5000);
+  }
+
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          {this.state.show ? <BurgerBuilder /> : null}
         </Layout>
       </div>
     );
