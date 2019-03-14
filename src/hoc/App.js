742d9a1e91f32da062c34cc252acbfaead69dffc
Rohit Kumawat <rkumawat125@gmail.com>
2019-03-15 01:35:05 +0530

    updated app.js file

--- src/hoc/App.js
+++ src/hoc/App.js
@@ -4,21 +4,11 @@ import Layout from './Layout/Layout';
 import BurgerBuilder from '../containers/BurgerBulder/BurgerBuilder';
 
 class App extends Component {
-  state = {
-    show: true
-  }
-  
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({show: false})
-    }, 5000);
-  }
-
   render() {
     return (
       <div>
         <Layout>
-          {this.state.show ? <BurgerBuilder /> : null}
+          <BurgerBuilder />
         </Layout>
       </div>
     );
