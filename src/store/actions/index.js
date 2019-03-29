10a6fd2af26963e0bd008b989f1aca49f3940dff
Rohit Kumawat <rkumawat125@gmail.com>
2019-03-29 17:27:47 +0530

    fetching orders with redux

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,3 @@
 export { addIngredients, removeIngredients, initIngredients } from './burgerBuilder';
 
-export { burgerPurchase, purchaseInit } from './order';
+export { burgerPurchase, purchaseInit, fetchOrders } from './order';
