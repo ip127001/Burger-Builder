25b2addc6c3c06ec8e9cfafb1968f2f36db40d2e
Rohit Kumawat <rkumawat125@gmail.com>
2019-04-06 22:59:39 +0530

    added lazy loading

--- src/hoc/App.js
+++ src/hoc/App.js
@@ -3,13 +3,25 @@ import { connect } from 'react-redux';
 
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 
+import asyncComponent from '../hoc/asyncComponent/asyncComponent';
+
 import Layout from './Layout/Layout';
 import BurgerBuilder from '../containers/BurgerBulder/BurgerBuilder';
-import Checkout from '../containers/Checkout/Checkout';
-import Orders from './../containers/Orders/Orders';
-import Auth from './../containers/Auth/Auth';
 import Logout from './../containers/Auth/Logout/logout';
 import * as action from '../store/actions/index';
+
+const asyncCheckout = asyncComponent(() => {
+  return import('../containers/Checkout/Checkout');
+})
+
+const asyncOrders = asyncComponent(() => {
+  return import('../containers/Orders/Orders');
+})
+
+const asyncAuth = asyncComponent(() => {
+  return import('../containers/Auth/Auth');
+})
+
 
 class App extends Component {
   componentDidMount() {
@@ -19,7 +31,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path="/auth" exact component={Auth} />
+        <Route path="/auth" exact component={asyncAuth} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
@@ -28,9 +40,9 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/auth" exact component={Auth} />
-          <Route path="/orders" component={Orders} />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/auth" exact component={asyncAuth} />
+          <Route path="/orders" component={asyncOrders} />
           <Route path="/logout" exact component={Logout} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/" />
